0967f458414f7816fc51d82b90fff94e0330d02c
Aaron Ruby <amsruby@live.com>
2018-07-31 20:04:07 +0000

    Changed vim settings

--- vim/install.sh
+++ vim/install.sh
@@ -7,7 +7,18 @@
 
 git clone https://github.com/xmementoit/vim-ide.git
 cd vim-ide
+
+# Get rid of bundles that we don't want 
 rm -rf bundle/minibufexpl
+rm -rf bundle/vim-lawrencium
+rm -rf bundle/vim-fugitive
+rm -rf bundle/gundo
+rm -rf bundle/vim-easymotion
+rm -rf bundle/yankring
+rm -rf bundle/snipmate
+
+rm -rf bundle/vim-expand-region
+rm -rf bundle/vimwiki
 
 cp ../vimrc vimrc
 ./installVim.sh
